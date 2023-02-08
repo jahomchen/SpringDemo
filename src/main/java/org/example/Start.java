4ca6b8af44f548fb55a04be2499907dc330a790b
陈家鸿 <chenjiahong@beisen.com>
2023-02-08 16:58:58 +0800

    add

--- src/main/java/org/example/Start.java
+++ src/main/java/org/example/Start.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.example.web.AppConfig;
+import org.example.web.model.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
@@ -10,6 +11,13 @@ public class Start {
         String[] beanDefinitionNames = run.getBeanDefinitionNames();
         for (String beanDefinitionName : beanDefinitionNames) {
             System.out.println(beanDefinitionName);
+        }
+
+        System.out.println("================================================");
+
+        String[] beanNamesForType = run.getBeanNamesForType(User.class);
+        for (String s : beanNamesForType) {
+            System.out.println(s);
         }
     }
 }
